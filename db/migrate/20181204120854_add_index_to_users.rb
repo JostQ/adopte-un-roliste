cad4458187e55ad1a2409f4052777363ae641263
CapellutoY <yossi.capelluto@gmail.com>
2018-12-04 14:56:26 +0100

    migrate part 2

--- db/migrate/20181204120854_add_index_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddIndexToUsers < ActiveRecord::Migration[5.2]
-  def change
-    add_column :users, :string
-    add_index :users, :string
-  end
-end
--- db/migrate/20181204120958_add_index_to_game_session.rb
+++
@@ -1,6 +0,0 @@
-class AddIndexToGameSession < ActiveRecord::Migration[5.2]
-  def change
-    add_column :game_sessions, :string
-    add_index :game_sessions, :string
-  end
-end
