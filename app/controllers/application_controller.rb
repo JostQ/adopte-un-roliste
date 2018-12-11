0972cfaa1795b22c1275197a7c280a749e7d62f8
Spliit <mcuville@gmail.com>
2018-12-11 22:17:37 +0100

    fix de la description

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:pseudo])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:pseudo, :password, :description])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:pseudo, :description, :password, :current_password])
   end
 end
