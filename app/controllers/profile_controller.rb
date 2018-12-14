c7d86c276d4861f1226d335be664e9b5251c5aa9
JostQ <dremsis64@hotmail.fr>
2018-12-14 11:53:28 +0100

    oups j'ai oublié de changer de branch, début d'ajax

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,6 +1,18 @@
 class ProfileController < ApplicationController
+  before_action :user_exist?
+
   def show
     @user = User.find(params[:id])
     @comments = Comment.where(recipient_id: params[:id])
+  end
+
+  private
+
+  def user_exist?
+    if params[:id]
+      unless User.find_by(id: params[:id])
+        redirect_to root_path
+      end
+    end
   end
 end
